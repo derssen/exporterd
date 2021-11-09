2d60cee1b25046443d56059fda93b48d636d4d92
derssen <78916246+derssen@users.noreply.github.com>
2021-11-09 17:04:56 +0200

    Update wallet.sh

--- wallet.sh
+++ wallet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-echo $(cat /usr/local/bin/wallet/ex) $(/usr/local/bin/wallet/1.sh) > /usr/local/bin/wallet/value/1.prom
+echo "amount_iron{iron="iron"}" $(cd /root/ironfish/ironfish-cli && yarn ironfish accounts:balance | head -n3 |  awk '{print $6}' | sed 's/\.//g') > /home/exporter/value/2.prom
 sleep 30
 done
